197bef595124d4adc64d499899dee03ca9441e2a
Aykut Gören <79761506+aykutgoren@users.noreply.github.com>
2025-07-01 17:37:48 +0100

    Revert "Feature/docker setup"

--- backend/app/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .routes import router  # Adjust if your router is elsewhere
-
-__all__ = ["router"]
--- backend/app/db/base.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.ext.declarative import DeclarativeMeta
-
-# Create a base class for all models to inherit from
-# `Base` will be used as the base class for SQLAlchemy ORM models
-Base: DeclarativeMeta = declarative_base()
